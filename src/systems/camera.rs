36e196cb8447ec9d3f7fc9f9682f8b01b058549d
Michael P. Jung <michael.jung@terreon.de>
2023-01-08 21:25:58 +0100

    Add .editorconfig and convert all files to use LF instead of CRLF

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::{Camera, Query, Transform, Without};
 
-use crate::components::{player::Player};
+use crate::components::player::Player;
 
 pub fn camera_system(
     mut camera_query: Query<(&Camera, &mut Transform, Without<Player>)>,
@@ -8,7 +8,9 @@ pub fn camera_system(
 ) {
     let (_, player_transform, _) = player_query.single();
     if let Ok((_, mut transform, _)) = camera_query.get_single_mut() {
-        transform.translation.x = player_transform.translation.x.clamp(-1920.0, 1920.0);
-        transform.translation.y = player_transform.translation.y.clamp(-1080.0, 1080.0);
+        //transform.translation.x = player_transform.translation.x.clamp(-1920.0, 1920.0);
+        //transform.translation.y = player_transform.translation.y.clamp(-1080.0, 1080.0);
+        transform.translation.x = player_transform.translation.x;
+        transform.translation.y = player_transform.translation.y;
     }
 }
